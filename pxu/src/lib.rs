d59dbbb5c030288deaa9011bc3943cbc42427fef
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2023-03-25 15:15:09 +0100

    Make interpolation pub

--- pxu/src/lib.rs
+++ pxu/src/lib.rs
@@ -1,9 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-// mod app;
-// mod frame_history;
-mod interpolation;
+pub mod interpolation;
 pub mod kinematics;
 mod nr;
 pub mod pxu;
-// pub use app::TemplateApp;
